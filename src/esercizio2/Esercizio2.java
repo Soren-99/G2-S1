d07eddd84e2f5e43b3390958a0e268e7ceb1d9bf
Persiano99 <sorenrahimi99@gmail.com>
2024-03-05 15:57:29 +0100

    es 2

--- src/esercizio2/Esercizio2.java
+++ src/esercizio2/Esercizio2.java
@@ -1,7 +1,31 @@
 package esercizio2;
 
+import java.util.Scanner;
+
 public class Esercizio2 {
     public static void main(String[] args){
 
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Inserisci un numero compreso tra 0 e 3: ");
+        int numero = scanner.nextInt();
+
+        switch (numero){
+            case 0:
+                System.out.println("Zero");
+                break;
+            case 1:
+                System.out.println("Uno");
+                break;
+            case 2:
+                System.out.println("Due");
+                break;
+            case 3:
+                System.out.println("Tre");
+                break;
+            default:
+                System.out.println("Errore");
+        }
+        scanner.close();
     }
 }
